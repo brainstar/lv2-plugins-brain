b8c73d166431dfbad7a3aac0d603c51ee08261f4
Christian Masser <christian.masser@gmail.com>
2020-06-27 11:23:44 +0200

    Write MovingAverage::pushData

--- pan4.cpp
+++ pan4.cpp
@@ -39,6 +39,58 @@ public:
 
 	// Push data points into the average filter
 	void pushData(int value, int length = 1) {
+		// Sanity check
+		if (length < 1) return;
+
+		// If you're expecting lengths > iSize, youshould write a small
+		// shortcut here to reset all vectors to the designated values
+		// As this is an RT application, there is no reason to
+		// anticipate this kind of behaviour and this comparison would
+		// slow the programm down a little bit
+
+		// Prevent buffer underrun
+		if (ptrFill == 0) {
+			vecSum[0] = vecSum[iSize - 1] - vecData[0] + value;
+			vecData[0] = value;
+			vecSumScaled[0] = (float) vecSum[0] / (float) iSize;
+			length--;
+			ptrFill++;
+		}
+
+		// No buffer overrun?
+		int target = ptrFill + length;
+		if (target <= iSize) {
+			// Just copy all values
+			for (; ptrFill < target; ptrFill++) {
+				vecSum[ptrFill] = vecSum[ptrFill - 1] - vecData[ptrFill] + value;
+				vecData[ptrFill] = value;
+				vecSumScaled[ptrFill] = (float) vecSum[ptrFill] / (float) iSize;
+			}
+			// If necessary reset ptrFill
+			if (ptrFill == iSize) ptrFill = 0;
+		} else {
+			// Rescale target to [0, iSize[
+			target -= iSize;
+
+			// Fill in from [ptrFill, iSize[
+			for (; ptrFill < iSize; ptrFill++) {
+				vecSum[ptrFill] = vecSum[ptrFill - 1] - vecData[ptrFill] + value;
+				vecData[ptrFill] = value;
+				vecSumScaled[ptrFill] = (float) vecSum[ptrFill] / (float) iSize;
+			}
+			// Fill in the 0 (buffer underrun prevention)
+			// 0 is filled, because of "if (target <= iSize)"
+			vecSum[0] = vecSum[iSize - 1] - vecData[0] + value;
+			vecData[0] = value;
+			vecSumScaled[0] = (float) vecSum[0] / (float) iSize;
+
+			// Fill in the rest
+			for (ptrFill = 1; ptrFill < target; ptrFill++) {
+				vecSum[ptrFill] = vecSum[ptrFill - 1] - vecData[ptrFill] + value;
+				vecData[ptrFill] = value;
+				vecSumScaled[ptrFill] = (float) vecSum[ptrFill] / (float) iSize;
+			}
+		}
 	}
 
 	// Get data point out of the filter and increment ptrRead
